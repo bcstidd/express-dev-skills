d15ee16b15c99f0266b7be88af5c149f7c117cd1
bcstidd <bc.stidd@gmail.com>
2023-01-11 17:05:06 -0800

    Lab complete

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,18 +2,39 @@ const Skill = require('../models/skill');
 
 module.exports = {
     index,
-    show
+    show,
+    new: newSkill,
+    create,
+    delete: deleteSkill
   };
   
 function index(req, res) {
     res.render('skills/index', {
-      skills: Skill.getAll()
+      skills: Skill.getAll(),
+      title: 'All Skills'
     });
+  }
+
+  function create(req, res) {
+    console.log(req.body);
+    // The model is responsible for creating data
+    Skill.create(req.body);
+    // Do a redirect anytime data is changed
+    res.redirect('/skills');
   }
 
   function show(req, res) {
     res.render('skills/show', {
-      skill: Skill.getOne(req.params.id)
+      skill: Skill.getOne(req.params.id),
+      title: 'Skill Details'
     })
   }
  
+  function newSkill(req, res) {
+    res.render('skills/new', { title: 'New Skill' });
+  }
+
+function deleteSkill(req, res) {
+  Skill.deleteOne(req.params.id);
+  res.redirect('/skills');
+}
